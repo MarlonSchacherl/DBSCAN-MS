93b25b209ae90ec826691148e1c9fbaec15ee732
Tesseracct <93708623+Tesseracct@users.noreply.github.com>
2025-09-13 16:12:20 +0200

    Fix SWNQA bugs.

--- src/main/scala/algorithm/SWNQA.scala
+++ src/main/scala/algorithm/SWNQA.scala
@@ -9,33 +9,34 @@ import utils.Distance.euclidean
 
 case object SWNQA {
   def apply(objects: RDD[DataPoint], epsilon: Float, seed: Int = 42): RDD[(DataPoint, List[DataPoint])] = {
-    val count = objects.count().toInt
-    require(count > -1, "Number of objects must be smaller than Int.MaxValue")
-
-    val rng = new Random(seed)
-    val dimension = rng.nextInt(objects.first().dimensions)
-    val sortedObjects = objects.map((_, List[DataPoint]())).sortBy(point => point._1.vectorRep(dimension))
-
-    sortedObjects.mapPartitions(iter => {
-      val points = iter.toArray
+    objects.mapPartitions(iter => {
+      val ps = iter.toArray.map((_, List[DataPoint]()))
+      val rng = new Random(seed)
+      val dimension = rng.nextInt(ps.head._1.dimensions)
+      val points = ps.sortBy(point => point._1.vectorRep(dimension))
 
       for (l <- points.indices) {
         val lPoint = points(l)._1
         val searchRegion = lPoint.vectorRep.map(x => (x - epsilon, x + epsilon))
 
         var u = l
-        val uPoint = points(u)._1
-        while (u < points.length && uPoint.vectorRep(dimension) - lPoint.vectorRep(dimension) > epsilon) {
+        var uPoint = points(u)._1
+        while (u < points.length && uPoint.vectorRep(dimension) - lPoint.vectorRep(dimension) <= epsilon) {
           if (inSearchRegion(searchRegion, uPoint) && lPoint.distance(uPoint, euclidean) <= epsilon) {
-            points(u) +: points(l)._2
-            points(l) +: points(u)._2
+            points(l) = (points(l)._1, points(u) +: points(l)._2)
+            points(u) = (points(u)._1, points(l) +: points(u)._2)
           }
+
           u = u + 1
+          if (u < points.length) {
+            uPoint = points(u)._1
+          }
         }
       }
       points.iterator
     })
   }
+
   def inSearchRegion(searchRegion: Array[(Float, Float)], point: DataPoint): Boolean = {
     for (i <- searchRegion.indices) {
       if (point.vectorRep(i) < searchRegion(i)._1 || point.vectorRep(i) > searchRegion(i)._2) {
@@ -44,6 +45,4 @@ case object SWNQA {
     }
     true
   }
-
-
 }
